87f78544e2746687629ce0c5fdd1bcecb694caef
Daqo Lee <2662114014@qq.com>
2018-12-29 15:33:18 +0800

    添加Joint.c文件

---
+++ RC_Templates/Control/Joint.h
@@ -0,0 +1,34 @@
+/**
+  ************************************* Copyright ******************************   
+  * (C) Copyright 2018,Daqo Lee,China, GCU.
+  *		All Rights Reserved
+  *
+  * By(ZHE WOLF TEAM OF GCU ROBOT)
+  * https://github.com/GCUWildwolfteam
+  *
+  * FileName   : Joint.h   
+  * Version    : v1.0		
+  * Author     : Daqo Lee			
+  * Date       : 2018-12-29         
+  * Description:    
+  * Function List:  
+  	1. ....
+  	   <version>: 		
+  <modify staff>:
+  		  <data>:
+   <description>:  
+  	2. ...
+  ******************************************************************************
+ */
+
+
+
+#ifndef __JOINT_H_
+#define __JOINT_H_
+
+
+#endif
+
+
+ 
+
